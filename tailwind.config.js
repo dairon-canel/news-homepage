34334531c4e1de461af5962df2c51f3d1527a634
Dairon Canel <98697567+RyuzakCoder@users.noreply.github.com>
2022-12-17 19:54:10 -0500

    feat: set up project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  purge: ['./public/index.html'],
+  theme: {
+    extend: {},
+    colors: {
+      softOrange: 'hsl(35, 77%, 62%)',
+      softRed: 'hsl(5, 85%, 63%)',
+      offWhite: 'hsl(36, 100%, 99%)',
+      grayishBlue: 'hsl(233, 8%, 79%)',
+      darkGrayishBlue: 'hsl(236, 13%, 42%)',
+      veryDarkBlue: 'hsl(240, 100%, 5%)',
+      white: 'hsl(0, 100%, 100%)',
+      noColor: 'hsla(0, 0%, 0%, 0)',
+    },
+    fontWeight: {
+      400: '400',
+      700: '700',
+      800: '800',
+    },
+  },
+  plugins: [],
+};
