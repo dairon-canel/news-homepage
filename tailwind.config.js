67b7ca6c197fbe03aa9b63d17a3afa4a2b93d2ea
Dairon Canel <98697567+RyuzakCoder@users.noreply.github.com>
2022-12-18 20:25:00 -0500

    fix: mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,25 @@ module.exports = {
   content: ['./public/index.html'],
   purge: ['./public/index.html'],
   theme: {
-    extend: {},
-    colors: {
-      softOrange: 'hsl(35, 77%, 62%)',
-      softRed: 'hsl(5, 85%, 63%)',
-      offWhite: 'hsl(36, 100%, 99%)',
-      grayishBlue: 'hsl(233, 8%, 79%)',
-      darkGrayishBlue: 'hsl(236, 13%, 42%)',
-      veryDarkBlue: 'hsl(240, 100%, 5%)',
-      white: 'hsl(0, 100%, 100%)',
-      noColor: 'hsla(0, 0%, 0%, 0)',
-    },
-    fontWeight: {
-      400: '400',
-      700: '700',
-      800: '800',
+    extend: {
+      screens: {
+        lg: '1130px',
+      },
+      colors: {
+        softOrange: 'hsl(35, 77%, 62%)',
+        softRed: 'hsl(5, 85%, 63%)',
+        offWhite: 'hsl(36, 100%, 99%)',
+        grayishBlue: 'hsl(233, 8%, 79%)',
+        darkGrayishBlue: 'hsl(236, 13%, 42%)',
+        veryDarkBlue: 'hsl(240, 100%, 5%)',
+        white: 'hsl(0, 100%, 100%)',
+        noColor: 'hsla(0, 0%, 0%, 0)',
+      },
+      fontWeight: {
+        400: '400',
+        700: '700',
+        800: '800',
+      },
     },
   },
   plugins: [],
